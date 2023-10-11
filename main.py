5d372c6f9dbecc2e7c1efb260ecfcc2ae32d9f01
narhzih <omosekejiolawale@gmail.com>
2023-10-11 14:36:46 +0100

    Created basic files

---
+++ main.py
@@ -0,0 +1,41 @@
+import requests
+
+# Details to make API request
+BLOCKONOMICS_API_URL = "https://www.blockonomics.co/api"
+API_KEY = "amqZYI11DDt9n1bHKTvLK78ZKwQw6jul8JtnY6rSNBY"
+
+
+# Function to get transaction output value
+def get_transaction_output_value(txid, address):
+    headers = {
+        "Authorization": f"Bearer {API_KEY}"
+    }
+    response = requests.get(f"{BLOCKONOMICS_API_URL}/tx_detail?txid={txid}", headers=headers)
+
+    if response.status_code != 200:
+        return None
+
+    transaction = response.json()
+
+    for output in transaction.get('vout', []):
+        if output['address'] == address:
+            # Convert the amount from Satoshis to Bitcoins
+            return output['value'] / 1e8
+
+    return None
+
+
+# Main function
+def main():
+    txid = input("Enter the Bitcoin transaction ID: ")
+    address = input("Enter the Bitcoin address: ")
+
+    output_value = get_transaction_output_value(txid, address)
+    if output_value is not None:
+        print(output_value)
+    else:
+        print("Transaction or Address not found in given transaction")
+
+
+if __name__ == "__main__":
+    main()
